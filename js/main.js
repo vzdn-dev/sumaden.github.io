2a3935c404c03ff6adb563565a64c267f3a6c189
vzdn-dev <77475833+vzdn-dev@users.noreply.github.com>
2024-01-25 16:43:12 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -326,7 +326,7 @@ $(function(){
 		
 		var links2 = [
 			["お申込みフォーム", "https://smaden.com/co2zero/applicationselect"],
-			["スマート通電 by スマ電のご案内", "https://smaden.com/partner"],
+			["特定商取引法に基づく表記", "https://smaden.com/tokuteisyotorihiki"],
 			["グリラボ", "https://gurilabo.igrid.co.jp/"],
 			["運営会社", "https://smaden.com/info/company.html"],
 			["個人情報保護方針", "https://smaden.com/info/privacy.html"],
